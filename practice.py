51f853eafa9f09eb74b63dc696dbb79474210da1
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 18:27:33 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,1 +1,17 @@
 #Return Values 
+
+e = math.exp(1.0)
+height = radius * math.sin(radians)
+
+def area(radius):
+    a = math.pi * radius**2
+    return a
+  
+def area(radius):
+    return math.pi * radius**2
+  
+ def absolute_value(x):
+     if x < 0:
+        return - x
+      if x > 0:
+        return x
